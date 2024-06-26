e5524b1e1933ad8b5a7d3bb4ac8946e527c4ad0b
bahiano-codes <wellington.videos1@gmail.com>
2024-06-25 21:48:47 -0300

    second-commit

--- app.py
+++ app.py
@@ -1,39 +1,41 @@
-from flask import Flask, request
+from app import create_app
+app = create_app()
 
-app = Flask(__name__)
+if __name__ == "__main__":
+    app.run(debug=True)
 
-@app.route('/tututubarao')
-def oque_eu_quiser():
-    return "<h5> ALguma coisa</h5>"
+# @app.route('/tututubarao')
+# def oque_eu_quiser():
+#     return "<h5> ALguma coisa</h5>"
 
-@app.route('/register', methods=['POST'])
-def registro():
-    nome = request.form['nome']
-    cpf = request.form['cpf']
-    return "O nome enviado é: " + nome + " e o cpf é: " + cpf
+# @app.route('/register', methods=['POST'])
+# def registro():
+#     nome = request.form['nome']
+#     cpf = request.form['cpf']
+#     return "O nome enviado é: " + nome + " e o cpf é: " + cpf
 
-@app.route("/")
-def pagina_inicial():
-    return '''<!DOCTYPE html>
-        <html lang="pt-br">
-        <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Formulário de Cadastro</title>
-        </head>
-        <body>
-            <h2>Formulário de Cadastro</h2>
-            <form action="/register" method="post">
-                <label for="nome">Nome:</label><br>
-                <input type="text" id="nome" name="nome" required><br><br>
+# @app.route("/")
+# def pagina_inicial():
+#     return '''<!DOCTYPE html>
+#         <html lang="pt-br">
+#         <head>
+#         <meta charset="UTF-8">
+#         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+#         <title>Formulário de Cadastro</title>
+#         </head>
+#         <body>
+#             <h2>Formulário de Cadastro</h2>
+#             <form action="/register" method="post">
+#                 <label for="nome">Nome:</label><br>
+#                 <input type="text" id="nome" name="nome" required><br><br>
                 
-                <label for="cpf">CPF DA PESSOA:</label><br>
-                <input type="text" id="cpf" name="cpf" placeholder="000.000.000-00" required><br><br>
+#                 <label for="cpf">CPF DA PESSOA:</label><br>
+#                 <input type="text" id="cpf" name="cpf" placeholder="000.000.000-00" required><br><br>
                 
-                <input type="submit" value="Enviar">
-            </form>
-        </body>
-        </html>'''
+#                 <input type="submit" value="Enviar">
+#             </form>
+#         </body>
+#         </html>'''
 
 
 
--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from . import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(120), nullable=False)
-    cpf = db.Column(db.String(11), nullable=False, unique=True)
+    cpf = db.Column(db.String(15), nullable=False, unique=True)
 
     def __repr__(self):
         return f'<User {self.nome}>'
