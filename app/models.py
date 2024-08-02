9df8281eab6790386f496550e6fe72e60bd46488
bahiano-codes <wellington.videos1@gmail.com>
2024-08-01 21:28:36 -0300

    adding other model

--- app/models.py
+++ app/models.py
@@ -7,3 +7,9 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.nome}>'
+
+class Escola(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(255), nullable=False)
+    data_fundacao = db.Column(db.DateTime(), nullable=False)
+    telefone = db.Column(db.String(14), nullable=True)
